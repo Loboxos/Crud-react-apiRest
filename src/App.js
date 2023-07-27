abb9d1186ffed5ceb705a1fc34bcebec0a9b3663
loboxos <vcristian528@gmail.com>
2023-07-27 17:05:48 -0300

    agregue tabla para los elementos de la api

---
+++ src/App.js
@@ -0,0 +1,58 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React,{useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Modal,ModalBody,ModalFooter,ModalHeader} from 'reactstrap';
+import axios from 'axios';
+
+
+function App() {
+  const baseUrl="http://localhost/apiFrameworks/";
+  const [data,setData]=useState([]);
+  
+  const peticionGet= async()=>{
+    await axios.get(baseUrl)
+    .then(response=>{
+      //console.log(response.data);
+      setData(response.data);
+    })
+  }
+  useEffect(()=>{
+    peticionGet();
+  },[])
+
+  return (
+    <div>
+     <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Lanzamiento</th>
+          <th>Desarrollador</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(tablas_framework=>(
+          <tr key={tablas_framework.id}>
+            <td>{tablas_framework.id}</td>
+            <td>{tablas_framework.nombre}</td>
+            <td>{tablas_framework.lanzamiento}</td>
+            <td>{tablas_framework.desarrollador}</td>
+          <td>
+          <button className="btn btn-primary" >Editar</button> {"  "}
+          <button className="btn btn-danger" >Eliminar</button>
+          </td>
+          </tr>
+        ))}
+
+
+      </tbody> 
+
+    </table>
+    </div>
+  );
+}
+
+export default App;
