2129d0b384bb247e8215c31e29fdb85e0965919a
loboxos <vcristian528@gmail.com>
2023-08-09 15:08:42 -0300

    hecho resolvi problema de key

--- src/App.js
+++ src/App.js
@@ -9,20 +9,111 @@ import axios from 'axios';
 function App() {
   const baseUrl="http://localhost/apiFrameworks/";
   const [data,setData]=useState([]);
-  
-  const peticionGet= async()=>{
+  const [modalInsertar, setModalInsertar]= useState(false);
+  const [modalEditar, setModalEditar]= useState(false);
+  const [modalEliminar, setModalEliminar]= useState(false);
+  const [frameworkSeleccionado, setFrameworkSeleccionado]=useState({
+    id: '',
+    nombre: '',
+    lanzamiento: '',
+    desarrollador: ''
+  });
+
+
+  const handleChange=e=>{
+    const {name, value}=e.target;
+    setFrameworkSeleccionado((prevState)=>({
+      ...prevState,
+      [name]: value
+    }))
+    console.log(frameworkSeleccionado);
+  }
+
+  const abrirCerrarModalInsertar=()=>{
+    setModalInsertar(!modalInsertar);
+  }
+  const abrirCerrarModalEditar=()=>{
+    setModalEditar(!modalEditar);
+  }
+
+  const abrirCerrarModalEliminar=()=>{
+    setModalEliminar(!modalEliminar);
+  }
+
+
+  const peticionGet=async()=>{
     await axios.get(baseUrl)
     .then(response=>{
-      //console.log(response.data)
       setData(response.data);
+    }).catch(error=>{
+      console.log(error);
     })
+  }
+
+  const peticionPost=async()=>{
+    var f = new FormData();
+    f.append("nombre", frameworkSeleccionado.nombre);
+    f.append("lanzamiento", frameworkSeleccionado.lanzamiento);
+    f.append("desarrollador", frameworkSeleccionado.desarrollador);
+    f.append("METHOD", "POST");
+    await axios.post(baseUrl, f)
+    .then(response=>{
+      setData(data.concat(response.data));
+      abrirCerrarModalInsertar();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+  const peticionPut=async()=>{
+    var f = new FormData();
+    f.append("nombre", frameworkSeleccionado.nombre);
+    f.append("lanzamiento", frameworkSeleccionado.lanzamiento);
+    f.append("desarrollador", frameworkSeleccionado.desarrollador);
+    f.append("METHOD", "PUT");
+    await axios.post(baseUrl, f, {params: {id: frameworkSeleccionado.id}})
+    .then(response=>{
+      var dataNueva = data;
+      dataNueva.forEach(tablas_framework => {
+        if (tablas_framework.id === frameworkSeleccionado.id) {
+          tablas_framework.nombre = frameworkSeleccionado.nombre;
+          tablas_framework.lanzamiento = frameworkSeleccionado.lanzamiento;
+          tablas_framework.desarrollador = frameworkSeleccionado.desarrollador;
+        }
+      });
+      setData(dataNueva);
+      abrirCerrarModalEditar();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+  const peticionDelete=async()=>{
+    var f = new FormData();
+    f.append("METHOD", "DELETE");
+    await axios.post(baseUrl, f, {params: {id: frameworkSeleccionado.id}})
+    .then(response=>{
+      setData(data.filter(tablas_framework=>tablas_framework.id!==frameworkSeleccionado.id));
+      abrirCerrarModalEliminar();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  const seleccionarFramework=(tablas_framework, caso)=>{
+    setFrameworkSeleccionado(tablas_framework);
+
+    (caso==="Editar")?
+    abrirCerrarModalEditar():
+    abrirCerrarModalEliminar()
   }
   useEffect(()=>{
     peticionGet();
   },[])
 
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
+<br />
+      <button className="btn btn-success" onClick={()=>abrirCerrarModalInsertar()}>Insertar</button>
+      <br/><br />
      <table className="table table-striped">
       <thead>
         <tr>
@@ -33,24 +124,89 @@ function App() {
           <th>Acciones</th>
         </tr>
       </thead>
-      <tbody>
-        {data.map(tablas_framework=>(
-          <tr key={tablas_framework.id}>
-            <td>{tablas_framework.id}</td>
-            <td>{tablas_framework.nombre}</td>
-            <td>{tablas_framework.lanzamiento}</td>
-            <td>{tablas_framework.desarrollador}</td>
-          <td>
-          <button className="btn btn-primary" >Editar</button> {"  "}
-          <button className="btn btn-danger" >Eliminar</button>
-          </td>
-          </tr>
-        ))}
-
+      <tbody>  
+      {console.log(data)}
+      {data.map((val,i)  => (
+    <tr key={i}>
+    <td>{val.id}</td>
+    <td>{val.nombre}</td>
+    <td>{val.lanzamiento}</td>
+    <td>{val.desarrollador}</td>
+    <td>
+      <button className="btn btn-primary" onClick={() => seleccionarFramework(val, "Editar")}>Editar</button>{" "}
+      <button className="btn btn-danger" onClick={() => seleccionarFramework(val, "Eliminar")}>Eliminar</button>
+    </td>
+  </tr>
+))}
 
       </tbody> 
 
     </table>
+    <Modal isOpen={modalInsertar}>
+      <ModalHeader>Insertar Framework</ModalHeader>
+      <ModalBody>
+        <div className="form-group">
+          <label>Nombre: </label>
+          <br />
+          <input type="text" className="form-control" name="nombre" onChange={handleChange}/>
+          <br />
+          <label>Lanzamiento: </label>
+          <br />
+          <input type="text" className="form-control" name="lanzamiento" onChange={handleChange}/>
+          <br />
+          <label>Desarrollador: </label>
+          <br />
+          <input type="text" className="form-control" name="desarrollador" onChange={handleChange}/>
+          <br />
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn btn-primary" onClick={()=>peticionPost()}>Insertar</button>{"   "}
+        <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button>
+      </ModalFooter>
+    </Modal>
+
+    <Modal isOpen={modalEditar}>
+      <ModalHeader>Editar Framework</ModalHeader>
+      <ModalBody>
+        <div className="form-group">
+          <label>Nombre: </label>
+          <br />
+          <input type="text" className="form-control" name="nombre" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.nombre}/>
+          <br />
+          <label>Lanzamiento: </label>
+          <br />
+          <input type="text" className="form-control" name="lanzamiento" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.lanzamiento}/>
+          <br />
+          <label>Desarrollador: </label>
+          <br />
+          <input type="text" className="form-control" name="desarrollador" onChange={handleChange} value={frameworkSeleccionado && frameworkSeleccionado.desarrollador}/>
+          <br />
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn btn-primary" onClick={()=>peticionPut()}>Editar</button>{"   "}
+        <button className="btn btn-danger" onClick={()=>abrirCerrarModalEditar()}>Cancelar</button>
+      </ModalFooter>
+    </Modal>
+
+    <Modal isOpen={modalEliminar}>
+        <ModalBody>
+        ¿Estás seguro que deseas eliminar el Framework {frameworkSeleccionado && frameworkSeleccionado.nombre}?
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={()=>peticionDelete()}>
+            Sí
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={()=>abrirCerrarModalEliminar()}
+          >
+            No
+          </button>
+        </ModalFooter>
+      </Modal>
+
     </div>
   );
 }
